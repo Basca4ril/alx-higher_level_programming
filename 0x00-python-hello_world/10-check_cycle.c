dbfaf5fe39254c6f5879db6c9f1c458b8d7cc34f
Basca4ril <basianchls@gmail.com>
2023-10-30 13:38:31 +0200

    added 10-check_cycle.c and lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for cycle in a linked list
+ * @list: list
+ * Return: 1 if theres cycle, 0 if theres not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1 = list, *ptr2 = list;
+
+	while(ptr1 && ptr2->next)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
