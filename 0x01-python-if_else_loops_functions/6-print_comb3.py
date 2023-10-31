78a4983e36348b0840e8e87a2505f893bea592a6
Basca4ril <basianchls@gmail.com>
2023-10-31 19:37:36 +0200

    added 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
-    for x in range(i +1, 10):
+    for x in range(i + 1, 10):
         if (i * 10) + x <= 88:
             print("{:d}{:d}".format(i, x), end=", ")
 print("{:d}".format(89))
