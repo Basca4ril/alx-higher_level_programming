ab7aaf88f9ac54ee38f579f488fb5711cc8270c1
Basca4ril <basianchls@gmail.com>
2023-11-02 16:29:32 +0200

    added 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == '__main__':
+    import hidden_4
 
-names = dir(hidden_4)
+    names = dir(hidden_4)
 
-fnames = [name for name in names if not name.startswith("__")]
-fnames.sort()
-for name in fnames:
-    print(name)
+    fnames = [name for name in names if not name.startswith('__')]
+
+    for name in sorted(fnames):
+        print(name)
