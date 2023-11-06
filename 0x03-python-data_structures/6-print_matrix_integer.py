330e8801cff596520f24e52f30c0aa5e58c3d4fa
Basca4ril <basianchls@gmail.com>
2023-11-06 15:07:20 +0200

    added task 6 file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,8 +5,8 @@ def print_matrix_integer(martix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j != len(matrix[i]) - 1:
-                end=" "
+                ends = ' '
             else:
-                end=""
-            print("{:d}".format(matrix[i][j]), end=" ")
+                ends = ''
+            print("{:d}".format(matrix[i][j]), end=ends)
         print()
