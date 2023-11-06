2667a9b00216fadce253efb288df2e50f639c951
Basca4ril <basianchls@gmail.com>
2023-11-06 13:20:26 +0200

    added task 4 file

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    newls = []
+    i = len(my_list)
+    if idx < 0 or idx > i:
+        return my_list
+    newls = my_list;
+    newls[idx] = element;
+    return newls
