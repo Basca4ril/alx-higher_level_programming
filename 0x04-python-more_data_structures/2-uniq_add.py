5fb6fd86ac338d13582666c08c11c80fdc12ff67
Basca4ril <basianchls@gmail.com>
2023-11-08 11:14:31 +0200

    added task 1 and task 2 files

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    ints = set()
+
+    for i in my_list:
+        if i not in ints:
+            ints.add(i)
+
+    tot = sum(ints)
+
+    return tot
+        
