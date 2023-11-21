903e14781368d4bee87e0f89583715f2206dc3c5
Basca4ril <basianchls@gmail.com>
2023-11-21 11:40:23 +0200

    added task 1 file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Definition of Square class
+"""
+
+
+class Square:
+    """
+    Square clas for square attributes and methods
+    """
+    def __init__(self, size):
+        """
+        Instantiates new clas
+
+        Parameters:
+        - size (int): Size of square
+        """
+        self.__size = size
+
+
+if __name__ == "__main__":
+    pass
