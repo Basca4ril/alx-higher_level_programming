37fa82d4c37cc59f25ec2170a065468c3bfc86ba
Basca4ril <basianchls@gmail.com>
2023-12-05 20:07:27 +0200

    added task files

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+appends to text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends string to text file
+    """
+    with open(filename, mode='', encoding='utf-8') as file:
+        chars = file.write(text)
+
+    return chars
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+returns json representation of an object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    returns json representation of an object
+    """
+    return json.dumps(my_obj)
