ce3a71c5b0785347439f787882de7a12919723e9
Basca4ril <basianchls@gmail.com>
2023-12-05 20:19:15 +0200

    added file 3

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,7 +8,7 @@ def append_write(filename="", text=""):
     """
     appends string to text file
     """
-    with open(filename, mode='', encoding='utf-8') as file:
+    with open(filename, mode='a', encoding='utf-8') as file:
         chars = file.write(text)
 
     return chars
